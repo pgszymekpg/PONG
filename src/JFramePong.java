f30a908652032284ae9db8ed00909b7654db6d25
Szymon <samek2002@gmail.com>
2023-10-30 21:52:10 +0100

    Adding

---
+++ src/JFramePong.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class JFramePong extends JFrame {
+    JPanelPong panel;
+    JFramePong(){
+        panel = new JPanelPong();
+        this.add(panel);
+        this.setVisible(true);
+        this.setSize(1280,1024);
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Pong");
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        JFramePong frame = new JFramePong();
+    }
+}
