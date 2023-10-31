d71341e1716ea6081fc084491569fb454fd882ad
Szymon <samek2002@gmail.com>
2023-10-31 19:35:49 +0100

    Adding

---
+++ src/Gameplay.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class Gameplay{
+
+
+    public Gameplay() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
+    public static JFrame frame = new JFrame();
     public static void main(String[] args) {
-        JFramePong frame = new JFramePong();
+        frame.setVisible(true);
+        frame.setSize(Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Pong");
+        frame.setLocationRelativeTo(null);
     }
 }
